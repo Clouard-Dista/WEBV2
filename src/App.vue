68e01b08f9ce2cd5280fa812016f5dd1835471be
Talloir Vincent <vincenttall@hotmail.fr>
2023-04-20 08:47:29 +0200

    test css

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,24 @@ import Skill from './components/Skill.vue'
     Hello world!
   </h1>
 
-  <Skill msg="LOL!" />
+  <Skill msg="LOL test!" />
+  
+  <nav :class="showMenu ? 'flex' : 'hidden'"
+      class="
+        flex-col
+        mt-8
+        space-y-4
+        md:flex
+        md:space-y-0
+        md:flex-row
+        md:items-center
+        md:space-x-10
+        md:mt-0
+      ">
+    <RouterLink class="text-sm font-bold text-gray-800 hover:text-blue-400" to="/">Home</RouterLink>
+    <RouterLink class="text-sm font-bold text-gray-800 hover:text-blue-400" to="/project">project</RouterLink>
+    <RouterLink class="text-sm font-bold text-gray-800 hover:text-blue-400" to="/skill">skill</RouterLink>
+    <RouterLink class="text-sm font-bold text-gray-800 hover:text-blue-400" to="/experience">experience</RouterLink>
+  </nav>
   <RouterView />
 </template>
